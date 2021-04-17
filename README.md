32f7dbe564480a560caee0d7c990a64d791b9471
范淼 <femiajayi705@gmail.com>
2021-04-17 10:24:03 +0800

    feat: Time complexity of a summation fn

---
+++ README.md
@@ -0,0 +1,3 @@
+# Write an algorithm that takes an array of numbers as input and calculates the sum of those numbers
+
+# Define the time complexity of the algorithm
--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Hello JS Algos");
+const sumNumInArrray = (array) => {
+  let sum = 0; // runs once
+  for (const val of array) {
+    // setup runs once
+
+    sum += val; // runs array.length => n times
+  }
+  return sum; // runs once
+};
+/**
+ *
+ * The time complexity of the is function can be calculated as:
+ * T = 1 + 1 + n + 1 = 3 + n = 3 + 1 * n
+ * T = 1 * n
+ * T = n => O(n) => Linear Time Complexity
+ */
+console.log(sumNumInArrray([1, 2, 3]));
